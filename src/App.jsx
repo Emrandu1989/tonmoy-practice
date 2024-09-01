1a3bcb3e0a639a41eae41abd4a32479ff63c8704
Emrandu1989 <emrandu1989@gmail.com>
2024-09-01 13:26:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [searchedValue, setSearchedValue] = useState("");
+  const [filterProducts, setFilteredProducts] = useState([]);
+  // console.log(inputValue);
+
+  useEffect(() => {
+    handleFilter();
+  }, [searchedValue]);
+
+  useEffect(() => {
+    fetch("https://y-taupe-chi.vercel.app/products?page=1&limit=40")
+      .then((res) => res.json())
+      .then((data) => {
+        const allProducts = data.products;
+        setData(allProducts);
+        setFilteredProducts(allProducts);
+      });
+  }, []);
+
+  const handleFilter = () => {
+    let result = data;
+    if (searchedValue) {
+      result = result.filter((product) =>
+        product.name.toLowerCase().includes(searchedValue.toLowerCase())
+      );
+    }
+    setFilteredProducts(result);
+  };
+
+  const handleSearchOnChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleSearched = () => {
+    setSearchedValue(inputValue);
+  };
+
+  return (
+    <div className="flex flex-col justify-center p-10">
+      <div className="my-10">
+        <div className="flex items-center justify-center ">
+          <input
+            value={inputValue}
+            onChange={handleSearchOnChange}
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full  outline-none max-w-xs"
+          />
+          <button
+            onClick={handleSearched}
+            className=" -ml-10 p-3 rounded-r-xl bg-purple-800 text-white "
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div>
+          {filterProducts.length === 0 ? (
+            <h2>No Product Found</h2>
+          ) : (
+              
+              <>
+                    <h1>Total Products: {filterProducts.length} </h1>
+                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+              {filterProducts.map((product) => (
+                <>
+                  <div
+                    className="card bg-base-100  shadow-xl p-5"
+                    key={product.name}
+                  >
+                    <figure>
+                      <img
+                        className="h-[400px] mb-2 w-full"
+                        src={product.image}
+                        alt={product.name}
+                      />
+                    </figure>
+
+                    <h2 className="card-title">Shoes!</h2>
+                    <p className="capitalize">name: {product.name}</p>
+                    <p className="capitalize">price: {product.price}</p>
+                    <p className="capitalize">
+                      description: {product.description}
+                    </p>
+                    <p className="capitalize">brand: {product.brand}</p>
+                    <p className="capitalize">category: {product.category}</p>
+                    <p className="capitalize">ratings: {product.ratings}</p>
+                  </div>
+                </>
+              ))}
+            </div>
+               </>
+
+          )}
+        </div>
+      </div>
+
+    
+    </div>
+  );
+};
+
+export default App;
