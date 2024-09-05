688133c7421020bbaf91e21eafddb0f263e37145
Emrandu1989 <emrandu1989@gmail.com>
2024-09-05 14:14:26 +0600

    sorting functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [searchedValue, setSearchedValue] = useState("");
   const [filterProducts, setFilteredProducts] = useState([]);
-  // console.log(inputValue);
+  const [categories, setCategories] = useState([]);
+  const [selectCategory, setSelectCategory] = useState([]);
 
+  // console.log(inputValue);
+  console.log(selectCategory);
   useEffect(() => {
     handleFilter();
-  }, [searchedValue]);
+  }, [searchedValue, selectCategory]);
 
   useEffect(() => {
     fetch("https://y-taupe-chi.vercel.app/products?page=1&limit=40")
@@ -20,6 +23,10 @@ const App = () => {
         const allProducts = data.products;
         setData(allProducts);
         setFilteredProducts(allProducts);
+        const uniqueCategory = Array.from(
+          new Set(allProducts.map((product) => product.category))
+        );
+        setCategories(uniqueCategory);
       });
   }, []);
 
@@ -30,6 +37,9 @@ const App = () => {
         product.name.toLowerCase().includes(searchedValue.toLowerCase())
       );
     }
+    if (selectCategory) {
+      result = result.filter((product) => product.category === selectCategory);
+    }
     setFilteredProducts(result);
   };
 
@@ -39,10 +49,13 @@ const App = () => {
   const handleSearched = () => {
     setSearchedValue(inputValue);
   };
+  const handleCategoryChange = (e) => {
+    setSelectCategory(e.target.value);
+  };
 
   return (
     <div className="flex flex-col justify-center p-10">
-      <div className="my-10">
+      <div className="my-10 flex justify-center items-center">
         <div className="flex items-center justify-center ">
           <input
             value={inputValue}
@@ -58,50 +71,54 @@ const App = () => {
             Search
           </button>
         </div>
+        <div>
+          <select onChange={handleCategoryChange} value={selectCategory}>
+            <option value="">Select Category</option>
+            {categories.map((category, index) => (
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="flex justify-center items-center">
         <div>
           {filterProducts.length === 0 ? (
             <h2>No Product Found</h2>
           ) : (
-              
-              <>
-                    <h1>Total Products: {filterProducts.length} </h1>
-                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-              {filterProducts.map((product) => (
-                <>
-                  <div
-                    className="card bg-base-100  shadow-xl p-5"
-                    key={product.name}
-                  >
-                    <figure>
-                      <img
-                        className="h-[400px] mb-2 w-full"
-                        src={product.image}
-                        alt={product.name}
-                      />
-                    </figure>
+            <>
+              <h1>Total Products: {filterProducts.length} </h1>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                {filterProducts.map((product) => (
+                  <>
+                    <div
+                      className="card bg-base-100  shadow-xl p-5"
+                      key={product.name}
+                    >
+                      <figure>
+                        <img
+                          className="h-[400px] mb-2 w-full"
+                          src={product.image}
+                          alt={product.name}
+                        />
+                      </figure>
 
-                    <h2 className="card-title">Shoes!</h2>
-                    <p className="capitalize">name: {product.name}</p>
-                    <p className="capitalize">price: {product.price}</p>
-                    <p className="capitalize">
-                      description: {product.description}
-                    </p>
-                    <p className="capitalize">brand: {product.brand}</p>
-                    <p className="capitalize">category: {product.category}</p>
-                    <p className="capitalize">ratings: {product.ratings}</p>
-                  </div>
-                </>
-              ))}
-            </div>
-               </>
-
+                      <h2 className="card-title">Shoes!</h2>
+                      <p className="capitalize">name: {product.name}</p>
+                      <p className="capitalize">price: {product.price}</p>
+                      <p className="capitalize">
+                        description: {product.description}
+                      </p>
+                      <p className="capitalize">brand: {product.brand}</p>
+                      <p className="capitalize">category: {product.category}</p>
+                      <p className="capitalize">ratings: {product.ratings}</p>
+                    </div>
+                  </>
+                ))}
+              </div>
+            </>
           )}
         </div>
       </div>
-
-    
     </div>
   );
 };
